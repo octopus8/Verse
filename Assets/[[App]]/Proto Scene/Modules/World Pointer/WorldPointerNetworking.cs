131e633898aea3c5efd841847bf5a82d593bf748
Brian A. Lee <brian.lee@metastreet.com>
2023-02-26 11:56:55 -1000

    Again, working on networking world pointer.

--- Assets/[[App]]/Proto Scene/Modules/World Pointer/WorldPointerNetworking.cs
+++ Assets/[[App]]/Proto Scene/Modules/World Pointer/WorldPointerNetworking.cs
@@ -28,9 +28,15 @@ public class WorldPointerNetworking : NetworkBehaviour {
 
     private void Start() {
         if (IsLocalPlayer) {
+            if (isServer) {
+                NetworkIdentity networkIdentity = GetComponent<NetworkIdentity>();
+                NetworkIdentity rootNetworkIdentity = GetComponentInParent<NetworkIdentity>();
+                networkIdentity.AssignClientAuthority(rootNetworkIdentity.connectionToClient);
+            }
+
             inputActions = new VerseInputActions();
             inputActions.Player.WorldPointer.Enable();
-            inputActions.Player.WorldPointer.performed += WorldPointer_performed;
+            inputActions.Player.WorldPointer.started += WorldPointerStarted;
         }
 
     }
@@ -43,19 +49,15 @@ public class WorldPointerNetworking : NetworkBehaviour {
     /// Callback called upon VoiceBroadcastGlobal input action.
     /// </summary>
     /// <param name="context">Input</param>
-    private void WorldPointer_performed(InputAction.CallbackContext context) {
-        if (context.ReadValueAsButton()) {
-            CmdOnPointerActiveChanged(true);
-        }
-        else {
-            CmdOnPointerActiveChanged(false);
-        }
+    private void WorldPointerStarted(InputAction.CallbackContext context) {
+        CmdOnPointerActiveChanged(context.ReadValueAsButton());
     }
 
 
 
     [Command]
     void CmdOnPointerActiveChanged(bool active) {
+        Debug.Log("CMD CALLED");
         pointerActivated = active;
     }
 
