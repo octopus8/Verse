8b92f81067ecd3dd896024b2e0d4d1340af8fb90
Brian A. Lee <brian.lee@metastreet.com>
2023-02-25 19:29:15 -1000

    Working on properly networking world pointer.

--- Assets/[[App]]/Proto Scene/Modules/World Pointer/WorldPointer.cs
+++ Assets/[[App]]/Proto Scene/Modules/World Pointer/WorldPointer.cs
@@ -7,18 +7,15 @@ public class WorldPointer : MonoBehaviour
     WorldPointerNetworking worldPointerNetworking;
     WorldPointerVisibility worldPointerVisibility;
 
-    private void Awake() {
-        worldPointerVisibility = GetComponent<WorldPointerVisibility>();
+    public bool IsLocalPlayer { private get; set; }
 
+    private void Start() {
+        worldPointerVisibility = GetComponent<WorldPointerVisibility>();
         GameObject networkRoot = GetComponentInParent<NetworkIdentity>().gameObject;
         worldPointerNetworking = networkRoot.AddComponent<WorldPointerNetworking>();
-    }
 
-
-    public bool IsLocalPlayer { set {
-            worldPointerNetworking.IsLocalPlayer = value;
-            worldPointerVisibility.IsLocalPlayer = value;
-        }
+        worldPointerNetworking.IsLocalPlayer = IsLocalPlayer;
+        worldPointerVisibility.IsLocalPlayer = IsLocalPlayer;
     }
 
 }
